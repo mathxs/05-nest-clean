8d031acb361df25674841a01a9bac7a43044bbe0
mathxs <matheusdsantosp@gmail.com>
2024-07-10 11:48:20 -0300

    aula inicial do 05 rocketseat

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/account')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handler(
+    @Body() body: { name: string; email: string; password: string },
+  ) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same email already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
