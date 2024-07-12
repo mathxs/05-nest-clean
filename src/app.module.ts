c3905e789454992396c26562df4da2c48aba79d6
mathxs <matheusdsantosp@gmail.com>
2024-07-12 09:19:45 -0300

    Ajuste de autenticacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthnenticateController } from './controllers/authenticate-controller'
+import { AuthnenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
@@ -14,7 +15,11 @@ import { AuthnenticateController } from './controllers/authenticate-controller'
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthnenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthnenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
