4a7b303c9397087449332acabcb2de86d19ef8df
mathxs <matheusdsantosp@gmail.com>
2024-07-17 15:02:57 -0300

    mudanca nas variaveis de ambiente

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { Env } from '../env'
 import { JwtStrategy } from './jwt.strategy'
 import { JwtAuthGuard } from './jwt-auth.guard'
 import { APP_GUARD } from '@nestjs/core/constants'
+import { EnvService } from '../env/env.service'
+import { EnvModule } from '../env/env.module'
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+      useFactory(env: EnvService) {
+        const privateKey = env.get('JWT_PRIVATE_KEY')
+        const publicKey = env.get('JWT_PUBLIC_KEY')
 
         return {
           signOptions: { algorithm: 'RS256' },
@@ -25,6 +26,10 @@ import { APP_GUARD } from '@nestjs/core/constants'
       },
     }),
   ],
-  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }, JwtStrategy],
+  providers: [
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    JwtStrategy,
+    EnvService,
+  ],
 })
 export class AuthModule {}
